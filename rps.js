06a3a41eb17e1008b0042df6625c8f4d3bdd598f
Connie <connieyzhu3@gmail.com>
2023-07-20 14:40:13 -0700

    Replace switch with if-else, used forEach for click event, copy code

--- rps.js
+++ rps.js
@@ -1,97 +1,60 @@
 let playerScore = 0;
-    let computerScore = 0;
-    const resultsDiv = document.querySelector('div');
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
 
-    const rockBtn = document.querySelector('.rock');
-    const paperBtn = document.querySelector('.paper');
-    const scissorsBtn = document.querySelector('.scissors');
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
 
-    function getComputerChoice(){
-        let computerChoice = Math.floor(Math.random()* 3);
-        switch(computerChoice){
-            case 0:
-                return "Rock";
-            case 1:
-                return "Paper";
-            case 2:
-                return "Scissors";
-        }
-    }
+function getComputerChoice(){
+    const computerChoice = ['Rock', 'Paper', 'Scissors'];
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+}
 
-    function play(playerSelection, computerSelection){
-        let roundResult;
-        console.log(playerSelection)
-        console.log(computerSelection);
-        switch(playerSelection){
-            case "Rock":
-                if(computerSelection === "Rock"){
-                    roundResult = "You Tie!";
-                }else if (computerSelection === "Paper"){
-                    roundResult = "You Lose! " + computerSelection + " beats " + playerSelection;
-                }else{
-                    roundResult = "You Win! " + playerSelection + " beats " + computerSelection;
-                }
-                break;
-            case "Paper":
-                if(computerSelection === "Rock"){
-                    roundResult = "You Win! " + playerSelection + " beats " + computerSelection;
-                }else if (computerSelection === "Paper"){
-                    roundResult = "You Tie!";
-                }else{
-                    roundResult = "You Lose! " + computerSelection + " beats " + playerSelection;
-                }
-                break;
-            case "Scissors":
-                if(computerSelection === "Rock"){
-                    roundResult = "You Lose! " + computerSelection + " beats " + playerSelection;
-                }else if (computerSelection === "Paper"){
-                    roundResult = "You Win! " + playerSelection + " beats " + computerSelection;
-                }else{
-                    roundResult = "You Tie!";
-                }
-                break;
-        }
-        return roundResult;
-    }
+function disableButtons(){
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    })
+}
 
-    function game(){ 
-        let roundResult = document.createElement('p');
-        let gameResult = document.createElement('p');
-
-        rockBtn.addEventListener('click', function(e){
-            roundResult.textContent = play('Rock', getComputerChoice())
-        });
-            paperBtn.addEventListener('click', function(e){
-            roundResult.textContent = play('Paper', getComputerChoice())
-        });
-            scissorsBtn.addEventListener('click', function(e){
-            roundResult.textContent = play('Scissors', getComputerChoice())
-        });
+function play(playerSelection, computerSelection){
+    let roundResult;
+    console.log(playerSelection)
+    console.log(computerSelection);
+    if((playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")){
         
-        while(playerScore < 5 && computerScore < 5){
-            if(roundResult.textContent.includes('Win')){
-                playerScore++;
-                roundResult.textContent += `${playerScore} + ':' + ${computerScore}`
-                resultsDiv.appendChild(roundResult);
-
-            }
-            if(roundResult.textContent.includes('Lost')){
-                computerScore++;
-                roundResult.textContent += `${playerScore} + ':' + ${computerScore}`
-                resultsDiv.appendChild(roundResult);
-            }
+        playerScore++;
+        roundResult = ("You Win! " + playerSelection + " beats " + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+        
+        if(playerScore == 5){
+            roundResult += "<br><br> You won the game!";
+            disableButtons();
         }
 
-
-        if(playerScore > computerScore){
-            gameResult.textContent = "Game Won! " + playerScore + ":" + computerScore;
-        }else if (playerScore < computerScore){
-            gameResult.textContent = "Game Lost! " + computerScore + ":" + playerScore; 
-        }else{
-            gameResult.textContent = "Game Tied! " + computerScore + ":" + playerScore;
+    }else if(playerSelection == computerSelection){
+        roundResult = ("You Tie!" 
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+    }else{
+        computerScore++;
+        roundResult = ("You Lose! " + computerSelection + " beats " + playerSelection
+        + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+        
+        if(computerScore == 5){
+            roundResult += "<br><br> You lost the game!";
+            disableButtons();
         }
-        resultsDiv.appendChild(gameResult);
-
     }
 
-    game();
+    document.getElementById('results').innerHTML = roundResult;
+
+    return;
+}
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        play(button.value, getComputerChoice());
+    })
+})
